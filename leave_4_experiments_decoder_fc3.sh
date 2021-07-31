2d6508e43987aa03f092319c309850514330a354
YounghyoPark <parkyh9492@gmail.com>
2021-07-31 10:38:39 +0900

    sync

---
+++ leave_4_experiments_decoder_fc3.sh
@@ -0,0 +1,9 @@
+python decoder_fc3.py --leave 4 -tech 0 --name leave_4_tech_0_decoder_fc3;
+python decoder_fc3.py --leave 4 -tech 1 --name leave_4_tech_1_decoder_fc3;
+python decoder_fc3.py --leave 4 -tech 2 --name leave_4_tech_2_decoder_fc3;
+python decoder_fc3.py --leave 4 -tech 3 --name leave_4_tech_3_decoder_fc3;
+python decoder_fc3.py --leave 4 -tech 4 --name leave_4_tech_4_decoder_fc3;
+python decoder_fc3.py --leave 4 -tech 5 --name leave_4_tech_5_decoder_fc3;
+python decoder_fc3.py --leave 4 -tech 6 --name leave_4_tech_6_decoder_fc3;
+python decoder_fc3.py --leave 4 -tech 7 --name leave_4_tech_7_decoder_fc3;
+python decoder_fc3.py --leave 4 -tech 8 --name leave_4_tech_8_decoder_fc3;
